f16333f85942968b50f84ec780d8843d1250c08b
Hamza Nadeem <hamzanadeem@codistan.org>
2024-07-10 13:06:13 +0500

    fixed: UI of Practice Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Page1 from './Page1';
-import Page2 from './Page2';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Practice from "./Practice";
+import Result from "./Result";
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Page1} />
-        <Route path="/Page2" component={Page2} />
+        <Route exact path="/" component={Practice} />
+        <Route path="/Result" component={Result} />
       </Switch>
     </Router>
   );
